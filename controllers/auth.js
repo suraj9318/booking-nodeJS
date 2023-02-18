1cec7377e5d2b26acf500242428c156ce6db3d90
suraj <surajmishra9318@gmail.com>
2023-02-18 22:26:27 +0530

    Register API, controller route and dcrypt done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,18 @@
-export const createHotel = async (req,res,next)=>{
-    const newHotel = new Hotel(req.body)
+import User from "../models/User.js"
+import bcrypt from "bcryptjs"
+export const register = async(req,res,next)=>{
     try {
-        const saveHotel = await newHotel.save()
-        res.status(201).json(saveHotel);
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        const newUser =await User.create({
+            username : req.body.username,
+            email : req.body.email,
+            password : hash,
+        })
+        await newUser.save();
+        res.status(201).json({success : true, msg : "User registered successfully."})
     } catch (error) {
-        next(error);
+        next(error)
     }
 }
