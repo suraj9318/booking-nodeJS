c636d56767820d81c3da6ea77fd6684e005368fa
suraj <surajmishra9318@gmail.com>
2023-02-19 22:49:00 +0530

    Room Schema and model created

---
+++ models/Room.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const Roomchema = mongoose.Schema({
+    title : {
+        type : String,
+        required : true,
+    },
+    price : {
+        type : Number,
+        required : true,
+    },
+    maxPeople : {
+        type : Number,
+        required : true,
+    }, 
+    desc : {
+        type : String,
+        required : true,
+    },
+    roomNumbers : [{number : Number, unavailableDates: [{type : [Date]}]}]
+
+
+},{
+    timestamps : true
+})
+
+const Room = mongoose.model('Room',Roomchema)
+
+export default User;
